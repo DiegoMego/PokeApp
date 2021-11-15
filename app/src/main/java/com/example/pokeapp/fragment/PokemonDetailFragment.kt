05d7b437d7cbdb8e13941efd35869943c43d1647
DiegoMego <30615834+DiegoMego@users.noreply.github.com>
2021-11-15 23:32:07 +0000

    fix: error when mapping.

--- app/src/main/java/com/example/pokeapp/fragment/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/pokeapp/fragment/PokemonDetailFragment.kt
@@ -27,7 +27,7 @@ class PokemonDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_list_pokemon, container, false)
+        return inflater.inflate(R.layout.fragment_pokemon_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/pokeapp/model/PokemonStat.kt
+++ app/src/main/java/com/example/pokeapp/model/PokemonStat.kt
@@ -1,6 +1,9 @@
 package com.example.pokeapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class PokemonStat (
+    @SerializedName("base_stat")
     val base_stat : Int,
     val stat : PokemonStatName
 )
